ef77b8e2c8b38fa24a4e8b3d9909958fab51914a
tingyuxuan <tingyuxuan169@163.com>
2019-12-23 20:24:34 +0800

    首页

---
+++ src/main/java/com/smxr/application/service/AccueilService.java
@@ -0,0 +1,11 @@
+package com.smxr.application.service;
+
+import com.smxr.application.dto.AccueilDTO;
+
+/**
+ * @author ZhangRongFei
+ * @date 2019/12/23 19:35
+ */
+public interface AccueilService {
+    public AccueilDTO findAll();
+}
